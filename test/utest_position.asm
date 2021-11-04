a2855013f15b72e3d0530db0534645dd7b68efa4
Fabian Schneider <81293687+Fabian-Schneider01@users.noreply.github.com>
2021-11-04 22:36:12 +0100

    version 1.0

---
+++ test/utest_position.asm
@@ -0,0 +1,55 @@
+.text
+#check if topbar existing and memory is filled with values
+test_draw_top:
+	la t0, init
+	lw t4, (t0)
+	beqz t4, utest_failed
+	lw t4, 12(t0)
+	beqz t4, utest_failed
+
+#draw a sidebar. if the values are being reset (because of delete_hit_loop and delete_miss_loop)
+#then the sidebars work as they should 
+test_hit:
+	jal draw_sidebar
+	jal draw.hit
+	lw t0, init
+	addi t0, t0, 60 
+	beqz t0, utest_failed
+	
+test_miss:
+	jal draw_sidebar
+	jal draw.miss
+	lw t0, init
+	addi t0, t0, 60
+	beqz t0, utest_failed
+	j utest_success
+	
+#draws the sidebar. draw_hit and draw_miss should delete it
+draw_sidebar:
+	li t4, 0x04FF06 
+	li t3, 12 
+	
+	li t2, 0
+	lw t0, init
+	addi t0, t0, 60 
+	
+	draw_bar:
+		sw t4, (t0)
+		addi t2, t2, 1
+		addi t0, t0, 64
+		bne t3, t2, draw_bar
+	ret
+	
+utest_failed:
+	li a0, -1
+	j utest_exit
+	
+#if a0 = 1 the unittest will print a success
+utest_success:
+	li a0, 1
+	j utest_exit
+
+.include "draw_pieces.asm"
+
+utest_exit:
+	addi zero, zero, 0
