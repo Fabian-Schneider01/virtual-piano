9f964e8a61db35a88b376e96c39dcaf0da165c9d
Fabian Schneider <81293687+Fabian-Schneider01@users.noreply.github.com>
2021-11-05 01:57:19 +0100

    version 1.0

---
+++ test/utest_saved.asm
@@ -0,0 +1,32 @@
+#automatically generated code by pressing save button
+#values will be compared in utest_tool to check wether buttons create the correct pitch
+#checked if values of duration, volume and instrument are in range
+.data
+melody:
+.word 60 62 64 65 67 69 71 70 68 66 63 61 
+
+.text
+initialize:
+li a7, 33
+li a1, 161
+li a2, 48
+li a3, 119
+la a4, melody
+li t0, 0 
+li t1, 44
+
+ret
+
+main:
+bge t1, t0, playMelody
+j exit 
+
+playMelody:
+lw a0, 0(a4)
+addi t0, t0, 4
+addi a4, a4, 4
+ecall 
+j main
+
+exit:
+addi zero, zero, 0
